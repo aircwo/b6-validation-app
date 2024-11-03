c3bd730f1422710e6196c96327719eccd449ab01
wortie <aircwo@gmail.com>
2024-11-03 17:22:10 +0000

    chore: initial commit with initialised repo

---
+++ src/main/java/com/example/b6_validation_app/B6ValidationAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.b6_validation_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class B6ValidationAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(B6ValidationAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/b6_validation_app/B6ValidationAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.b6_validation_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class B6ValidationAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
