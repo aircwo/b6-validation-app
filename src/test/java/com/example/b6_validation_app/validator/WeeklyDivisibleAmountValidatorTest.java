6b2c3c17e5b6e2d62ad8bf4141761110a6f47ba2
wortie <aircwo@gmail.com>
2024-11-03 19:05:52 +0000

    chore: expect is valid to be true on null frequency

--- src/test/java/com/example/b6_validation_app/validator/WeeklyDivisibleAmountValidatorTest.java
+++ src/test/java/com/example/b6_validation_app/validator/WeeklyDivisibleAmountValidatorTest.java
@@ -3,21 +3,29 @@ package com.example.b6_validation_app.validator;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.mock;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
 import com.example.b6_validation_app.common.RegularAmount;
 
 import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
 
 class WeeklyDivisibleAmountValidatorTest {
 
+  private WeeklyDivisibleAmountValidator validator;
+  private ConstraintValidatorContext context;
+
+  @BeforeEach
+  void setUp() {
+    validator = new WeeklyDivisibleAmountValidator();
+    context = mock(ConstraintValidatorContext.class);
+  }
+
   @Test
   void shouldImplementConstraintValidator() {
-    // given
-    WeeklyDivisibleAmountValidator validator = new WeeklyDivisibleAmountValidator();
-
-    // when
+    // given / when
     boolean implementsInterface = validator instanceof ConstraintValidator;
 
     // then
@@ -26,14 +34,26 @@ class WeeklyDivisibleAmountValidatorTest {
 
   @Test
   void shouldHaveIsValidMethod() throws NoSuchMethodException, SecurityException {
-    // given
-    WeeklyDivisibleAmountValidator validator = new WeeklyDivisibleAmountValidator();
-
-    // when
-    java.lang.reflect.Method isValidMethod = validator.getClass().getMethod("isValid", RegularAmount.class, javax.validation.ConstraintValidatorContext.class);
+    // given / when
+    java.lang.reflect.Method isValidMethod = validator.getClass()
+      .getMethod("isValid", RegularAmount.class, ConstraintValidatorContext.class);
 
     // then
     assertNotNull(isValidMethod);
     assertEquals(boolean.class, isValidMethod.getReturnType());
+  }
+
+  @Test
+  void shouldPassValidationWhenFrequencyIsNull() {
+    // given
+    RegularAmount regularAmount = new RegularAmount();
+    regularAmount.setAmount("100.00");
+    regularAmount.setFrequency(null);
+
+    // when
+    boolean isValid = validator.isValid(regularAmount, context);
+
+    // then
+    assertTrue(isValid);
   }
 }
