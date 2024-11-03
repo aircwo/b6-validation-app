3159d7c27d650e16f694226a16e07ee8a7263314
wortie <aircwo@gmail.com>
2024-11-03 17:47:28 +0000

    chore: add remaining enums to fix tests

--- src/main/java/com/example/b6_validation_app/common/Frequency.java
+++ src/main/java/com/example/b6_validation_app/common/Frequency.java
@@ -3,11 +3,19 @@ package com.example.b6_validation_app.common;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
+/**
+ * Frequency Enum outlining the accepted frequencies within a RegularAmount object.
+ * All args constructor removes bloat here with Frequency(weekMultiple) auto-generated.
+ */
 @Getter
 @AllArgsConstructor
 public enum Frequency {
   WEEK(1),
-  TWO_WEEK(2);
+  TWO_WEEK(2),
+  FOUR_WEEK(4),
+  MONTH(4),
+  QUARTER(13),
+  YEAR(52);
 
   private final int weekMultiple;
 
