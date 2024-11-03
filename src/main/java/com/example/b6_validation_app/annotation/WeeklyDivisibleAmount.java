dcabf4ec1ade415be9a9fc5b092d4e49adfc5ee3
wortie <aircwo@gmail.com>
2024-11-03 18:58:46 +0000

    chore: implement constraint validator with constraint on regular amount object

--- src/main/java/com/example/b6_validation_app/annotation/WeeklyDivisibleAmount.java
+++ src/main/java/com/example/b6_validation_app/annotation/WeeklyDivisibleAmount.java
@@ -1,3 +1,20 @@
 package com.example.b6_validation_app.annotation;
 
-public @interface WeeklyDivisibleAmount {}
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+import com.example.b6_validation_app.validator.WeeklyDivisibleAmountValidator;
+
+import jakarta.validation.Constraint;
+import jakarta.validation.Payload;
+
+@Constraint(validatedBy = WeeklyDivisibleAmountValidator.class)
+@Target({ElementType.TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+public @interface WeeklyDivisibleAmount {
+    String message() default "Amount must be divisible into whole pence when split into weekly payments";
+    Class<?>[] groups() default {};
+    Class<? extends Payload>[] payload() default {};
+}
--- src/test/java/com/example/b6_validation_app/annotation/WeeklyDivisibleAmountTest.java
+++ src/test/java/com/example/b6_validation_app/annotation/WeeklyDivisibleAmountTest.java
@@ -1,3 +1,44 @@
 package com.example.b6_validation_app.annotation;
 
-public class WeeklyDivisibleAmountTest {}
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.Set;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import com.example.b6_validation_app.common.Frequency;
+import com.example.b6_validation_app.common.RegularAmount;
+import com.example.b6_validation_app.validator.WeeklyDivisibleAmountValidator;
+
+import jakarta.validation.ConstraintViolation;
+import jakarta.validation.Validation;
+import jakarta.validation.Validator;
+import jakarta.validation.ValidatorFactory;
+
+class WeeklyDivisibleAmountTest {
+
+  private static Validator validator;
+
+  @BeforeAll
+  public static void setUpValidator() {
+    ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
+    validator = factory.getValidator();
+  }
+
+  @Test
+  void shouldUseAnnotationErrorMessageOnValidationError() {
+    // given
+    RegularAmount invalidAmount = new RegularAmount(); // could use a builder test factory
+    invalidAmount.setAmount("100.01");
+    invalidAmount.setFrequency(Frequency.TWO_WEEK);
+
+    // when
+    Set<ConstraintViolation<RegularAmount>> violations = validator.validate(invalidAmount);
+
+    // then
+    assertEquals(0, violations.size());
+    // todo
+  }
+}
